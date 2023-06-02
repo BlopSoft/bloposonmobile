72a7c2326bb3f3d899ec8d7b43388c9d865a4420
dibil228of <85364286+dibil228of@users.noreply.github.com>
2023-06-02 11:42:03 +0300

    Add files via upload

---
+++ app/src/main/java/io/blopsoft/bloposonmobile/MainActivity.java
@@ -0,0 +1,35 @@
+package io.blopsoft.bloposonmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.blopos);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://blopsoft.github.io/bloposmobile");
+
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
